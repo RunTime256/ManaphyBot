64de823c18071d4fee5a6ecdd8d2b9845407c3eb
RunTime256 <runtime256@gmail.com>
2019-07-11 22:55:31 -0500

    Session comments

--- src/main/java/sql/SessionFactory.java
+++ src/main/java/sql/SessionFactory.java
@@ -7,16 +7,34 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.Reader;
 
+/**
+ * Creates sessions for SQL statement execution
+ */
 public class SessionFactory
 {
     private static SqlSessionFactory factory;
 
+    private SessionFactory()
+    {
+    }
+
+    /**
+     * Initializes factory with a MyBatis config file
+     *
+     * @param config file name in resource directory
+     * @throws IOException if resource was not found
+     */
     public static void init(String config) throws IOException
     {
         Reader reader = Resources.getResourceAsReader(config);
         factory = new SqlSessionFactoryBuilder().build(reader);
     }
 
+    /**
+     * Gets a new session for SQL execution
+     *
+     * @return SQL session
+     */
     public static Session getSession()
     {
         return new Session(factory.openSession());
