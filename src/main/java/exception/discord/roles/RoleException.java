8077ddfde50b95d7de53705c9d78c58aa07767f2
RunTime256 <runtime256@gmail.com>
2019-07-11 22:58:03 -0500

    Discord exception comments

--- src/main/java/exception/discord/roles/RoleException.java
+++ src/main/java/exception/discord/roles/RoleException.java
@@ -2,8 +2,16 @@ package exception.discord.roles;
 
 import exception.discord.DiscordException;
 
+/**
+ * Thrown when an exception occurs with roles
+ */
 public class RoleException extends DiscordException
 {
+    /**
+     * Constructs with a message
+     *
+     * @param message the details of the exception
+     */
     public RoleException(String message)
     {
         super(message);
--- src/main/java/exception/discord/roles/UserDoesNotHaveRoleException.java
+++ src/main/java/exception/discord/roles/UserDoesNotHaveRoleException.java
@@ -1,7 +1,15 @@
 package exception.discord.roles;
 
+/**
+ * Thrown when a user does not have a role
+ */
 public class UserDoesNotHaveRoleException extends RoleException
 {
+    /**
+     * Constructs with a message
+     *
+     * @param message the details of the exception
+     */
     public UserDoesNotHaveRoleException(String message)
     {
         super(message);
